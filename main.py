35b87b75ce8354c155f4aa6d6aed37ce6aa187d8
Sebastian Chavarr <sec223@lehigh.edu>
2023-11-29 15:04:48 -0500

    handle animation loading better + simple mode flip implemented

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ TILE_SIZE = data["world"]["tile_data"]["size"]
 WINDOW_WIDTH = TILE_SIZE * data["world"]["window_data"]["tiles_across"]
 WINDOW_HEIGHT = TILE_SIZE * data["world"]["window_data"]["tiles_down"]
 
+TIMER_EVENT = pygame.event.custom_type()
+
 pygame.init() 
 
 display_window = pygame.display.set_mode((WINDOW_WIDTH * 2, WINDOW_HEIGHT * 2), 0, 32)
@@ -28,13 +30,14 @@ world = World()
 frame_start = 0
 frame_end = pygame.time.get_ticks()
 dt = frame_end - frame_start
+# every 10 seconds
+pygame.time.set_timer(TIMER_EVENT, 10000)
 # =====================================================
 # ============ MAIN GAME LOOP =========================
 # =====================================================
 while playing :
     frame_start = frame_end
     raw_window.fill((0,0,0))
-
     # ============ EVENT HANDLING ============
     # ------------ single keypress event polling ------------
     for event in pygame.event.get() :
@@ -45,6 +48,8 @@ while playing :
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+        if event.type == TIMER_EVENT :
+            world.mode = not world.mode
 
     # ------------ key held down event polling ------------
     keys = pygame.key.get_pressed()
@@ -53,9 +58,18 @@ while playing :
 
     # ============ RENDERING ============
     for coord in world.tile_map.keys() :
-        if world.tile_map[coord] == 0 :
-            raw_window.blit(world.tile_assets['base tile'], (TILE_SIZE * coord[0], TILE_SIZE * coord[1]))
+        if world.mode == 0 : # ** dark mode
+            if world.tile_map[coord] == 0 :
+                raw_window.blit(world.tile_assets['dark base tile'], (TILE_SIZE * coord[0], TILE_SIZE * coord[1]))
+        elif world.mode == 1 : # ** dark mode
+            if world.tile_map[coord] == 0 :
+                raw_window.blit(world.tile_assets['light base tile'], (TILE_SIZE * coord[0], TILE_SIZE * coord[1]))
+            
     
     scaled_window = pygame.transform.scale(raw_window, display_window.get_size())
     display_window.blit(scaled_window, (0,0))
     pygame.display.update()
+
+    frame_end = pygame.time.get_ticks()
+    dt = frame_end - frame_start
+    clock.tick(60)
--- utils.py
+++ utils.py
@@ -6,13 +6,15 @@ BASE_PATH = './assets/'
 # ============ MISC HELPER FUNCTIONS ==================
 # =====================================================
 def load_image(path):
-    img = pygame.image.load(BASE_PATH + path).convert()
+    img : Surface = pygame.image.load(BASE_PATH + path).convert()
     img.set_colorkey((0, 0, 0))
     return img
 
 def load_dir(path) :
     images = []
     for img_name in sorted(os.listdir(BASE_PATH + path)):
+        if img_name == ".DS_Store" :
+            continue
         images.append(load_image(path + '/' + img_name))
     return images
 
@@ -29,24 +31,35 @@ def stringFromTuple (t : tuple) -> str:
 # =====================================================
 class World() :
     def __init__(self) -> None:
-        # ========= OPEN FILE =========
+    # ========= OPEN FILE =========
         mapFile = open('data.json')
         data = json.load(mapFile)
         mapFile.close()
         world = data["world"]
-        # ========= LOAD TILE ASSETS =========
+    # ========= LOAD TILE ASSETS =========
         tiles = world["tile_assets"]
+        # ** Instance variable
         self.tile_assets = {}
         for key in tiles :
             self.tile_assets[key] = load_image(tiles[key])
-        # ========= LOAD GENERAL ASSETS =========
+    # ========= LOAD GENERAL ASSETS =========
         assets = world["assets"]
+        # ** Instance variable
         self.assets = {}
         for key in assets :
+            if key == "animations" :
+                continue
             self.assets[key] = load_image(assets[key])
-        # ========= HANDLE MAP =========
-        # the absence of a coordinate in "map" in the json indicates that it's just a blank tile
-        # NOTE : tile_map dict will have raw tuples as keys, while json will have strings of tuples as keys
+    # ========= LOAD ANIMATIONS =========
+        animations = assets["animations"]
+        # ** Instance variable
+        self.animations = {}
+        for key in animations :
+            self.animations[key] = load_dir(animations[key])
+    # ========= HANDLE MAP =========
+    # the absence of a coordinate in "map" in the json indicates that it's just a blank tile
+    # NOTE : tile_map dict will have raw tuples as keys, while json will have strings of tuples as keys
+        # ** Instance variable
         self.tile_map = {}
         for y in range(world["window_data"]["tiles_down"]) :
             for x in range(world["window_data"]["tiles_across"]) :
@@ -54,7 +67,7 @@ class World() :
                 if stringFromTuple((x, y)) not in world["map"].keys():
                     self.tile_map[(x, y)] = 0
                 
-        self.level_number = 0
+        self.mode = 0
 
 # =====================================================
 # ============ ENTITY CLASS ===========================
